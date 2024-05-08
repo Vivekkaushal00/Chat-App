24aa64ae978382461a281ee82d2d0f8d06f86b97
Vivek Kaushal <124867828+Vivekkaushal00@users.noreply.github.com>
2024-05-08 21:27:12 +0530

    image

--- lib/screens/Widgets/user_image_picker.dart
+++ lib/screens/Widgets/user_image_picker.dart
@@ -22,8 +22,8 @@ class _UserImagePickerState extends State<UserImagePicker> {
 
   void _pickImage() async {
     final pickedImage = await ImagePicker().pickImage(
-      source: ImageSource.camera,
-      imageQuality: 50,
+      source: ImageSource.gallery,
+      imageQuality: 100,
       maxWidth: 150,
     );
 
